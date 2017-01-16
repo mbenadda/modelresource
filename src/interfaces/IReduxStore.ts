1cb6b4f11897e5b96ae10f89e84f580a4f3a0771
Mehdi Benadda <me@mbenadda.com>
2017-01-16 03:31:26 +0100

    Update IReduxStore to be an Observable
    As we're performing rxjs operations on the store throughout our
    methods, it needs to be an extension of Observable, and defined
    as such.

--- src/interfaces/IReduxStore.ts
+++ src/interfaces/IReduxStore.ts
@@ -1,7 +1,8 @@
 import { IReduxAction } from './IReduxAction';
 import { IReduxState } from './IReduxState';
+import { Observable } from 'rxjs/Observable';
 
-export interface IReduxStore {
+export interface IReduxStore extends Observable<IReduxState> {
   dispatch: { (action: IReduxAction): void };
   getValue: { (): IReduxState };
 };
